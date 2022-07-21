5bf33a9771ba73c2ce9029981c0760c043bdc4ae
Laura <laura97green@gmail.com>
2022-07-21 11:08:52 +0200

    Add to Registration password and confirmed password

--- src/Components/Registration/registration.js
+++ src/Components/Registration/registration.js
@@ -3,24 +3,32 @@ import './registration.css';
 
 export default function Registration () {
     const initialState = {
-        firstname: '',
+        firstName: '',
         lastName: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: ''
     }
 
     const [form, setForm] = useState(initialState);
     const [addedForm, setAddedForm] = useState([]);
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
         const nextAddedForm = [...addedForm, form]
 
+        if (form.password === form.confirmPassword) {
+            console.log('match!')
+        }
         setAddedForm(nextAddedForm);
         setForm(initialState)
     }
 
-    console.log(form);
-    console.log(addedForm);
+    const handleFirstnameChange = (event) => setForm(state => ({ ...state, firstName: event.target.value }));
+    const handleLastnameChange = (event) => setForm(state => ({ ...state, lastName: event.target.value }));
+    const handleEmailChange = (event) => setForm(state => ({ ...state, email: event.target.value }));
+    const handlePasswordChange = (event) => setForm(state => ({ ...state, password: event.target.value }));
+    const handleConfirmPasswordChange = (event) => setForm(state => ({ ...state, confirmPassword: event.target.value }));
 
     return (
         <div className="registration">
@@ -29,29 +37,29 @@ export default function Registration () {
                 <fieldset className="registration__fieldset">
                     <label className="registration__label">
                         <p className="registration__paragraph">First Name</p>
-                        <input className="registration__input" type="text" onChange={firstname => setForm(state => ({ ...state, firstname: firstname.target.value }))} />
+                        <input className="registration__input" type="text" onChange={handleFirstnameChange} />
                     </label>
                     <label className="registration__label">
                         <p className="registration__paragraph">Last Name</p>
-                        <input className="registration__input" type="text" onChange={lastName => setForm(state => ({ ...state, lastName: lastName.target.value }))} />
+                        <input className="registration__input" type="text" onChange={handleLastnameChange} />
                     </label>
                     <label className="registration__label">
                         <p className="registration__paragraph">Email</p>
-                        <input className="registration__input" type="text" onChange={email => setForm(state => ({ ...state, email: email.target.value }))} />
+                        <input className="registration__input" type="text" onChange={handleEmailChange} />
                     </label>
                     <label className="registration__label">
                         <p className="registration__paragraph">Password</p>
-                        <input className="registration__input" type="password" minLength="8" required />
+                        <input className="registration__input" type="password" minLength="8" required onChange={handlePasswordChange} />
                     </label>
                     <label className="registration__label">
                         <p className="registration__paragraph">Confirm Password</p>
-                        <input className="registration__input" type="password" minLength="8" required />
+                        <input className="registration__input" type="password" minLength="8" required onChange={handleConfirmPasswordChange} />
                     </label>
                     
                 </fieldset>
                 <button className="registration__button button--submit" onClick={handleSubmit} >
-                        Submit
-                    </button>
+                    Submit
+                </button>
             </form>
         </div>
     )
